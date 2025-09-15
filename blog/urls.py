ac81f8068b4d8be38f344ed46379dfe08406d966
ASUS <ASUS@DESKTOP-NB7MT4D>
2025-09-15 12:36:24 +0400

    addedDeleteAndDetailsPage

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:post_id>/delete', views.post_delete, name='post_delete'),
     path('post/<int:post_id>', views.post_detail, name='post_detail'),
     # path('post/new', views.post_create, name='post_create'),
     # path('post/<int:id>/edit', views.post_edit, name='post_edit'),
-    # path('post/<int:id>/delete', views.post_delete, name='post_delete'),
     # path('search/', views.post_search, name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -22,3 +24,27 @@ def post_detail(request,post_id:int):
         return render(request,'blog/post_details.html',context={'post':post})
     else:
         return render(request,'blog/Notfound.html')
+
+
+# def delete(request,post_id):
+#     post=data.get_post(post_id)
+#     if post is not None:
+#         response = data.delete_post(post.id)
+#         if response:
+#             return HttpResponse('Deleted')
+#         else:
+#             return HttpResponse('204 No Content')
+#     else:
+#         return HttpResponse('404 Not Found')
+#
+@require_http_methods(["POST"])
+def post_delete(request, post_id):
+    post = data.get_post(post_id)
+    if post is not None:
+        deleted = data.delete_post(post.id)
+        if deleted:
+            return redirect('/')
+        else:
+            return HttpResponse(status=204)
+    else:
+        return HttpResponse("Post not found")
